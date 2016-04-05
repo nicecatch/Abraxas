cf5d0c36c22cbce2a999519aa543a45416d02b76
nicecatch <nicecatch@users.noreply.github.com>
2016-04-05 15:07:59 +0200

    Aggiungo Avg, Min e Max

--- runner/plc/PLC.py
+++ runner/plc/PLC.py
@@ -4,8 +4,11 @@ Created on 04 apr 2016
 @author: posiandr
 '''
 import snap7
+from snap7.snap7exceptions import Snap7Exception 
 from snap7.snap7types import areas
 from snap7 import util
+
+#import sys
 
 class PLC():
     
@@ -14,15 +17,27 @@ class PLC():
         self.name = name
         self.rack = rack
         self.slot = slot
-        self.array = [0] * 10
-        self.index = 0
-        self.avg = 0
-        self.max = 0
-        self.min = 0
+        self.connect()
     
-        self.snap7 = snap7.client.Client()
-        self.snap7.connect(ip, rack, slot)
-    
-    def readValue(self):
+    def read(self):
         read = self.snap7.read_area(areas['DB'], 105, 0, 16)
         return [util.get_int(read, 2), util.get_int(read, 4)]
+    
+    def readValue(self):
+        try:
+            return self.read()
+        except Snap7Exception as error: #Errore, provo una volta a ricollegarmi e a rileggere
+            try:
+                self.connect()
+                return self.read()
+            
+            except Snap7Exception as error:
+                print "Unexpected error: ", error
+                
+            
+    def connect(self):
+        try:
+            self.snap7 = snap7.client.Client()
+            self.snap7.connect(self.ip, self.rack, self.slot)
+        except Snap7Exception as error:
+            print "Unexpected error: ", error
--- runner/threader/Threader.py
+++ runner/threader/Threader.py
@@ -3,7 +3,7 @@ Created on 04 apr 2016
 
 @author: posiandr
 '''
-
+import numbers
 import threading
 import time
 #from snap7 import util
@@ -12,6 +12,7 @@ import time
 
 #from runner.plc import PLC
 
+READS_TO_LOG = 10
 
 class PLCReader (threading.Thread):
     def __init__(self, plc, config):
@@ -19,14 +20,62 @@ class PLCReader (threading.Thread):
         self.plc = plc
         self.config = config
         self.alive = True
+        
+        self.array = [0] * READS_TO_LOG
+        self.index = 0
+        self.avg = 0
+        self.max = 0
+        self.min = 0
+        self.latestValue = 0
     
     def run(self):
         while self.alive is True:
             read = self.plc.readValue()
-            print self.plc.name
-            print 'Values: 1) ' + str(read[0]) + '. 2) ' + str(read[1])
+            
+            readValue = -1
+            
+            if isinstance(read[0], numbers.Number):
+                readValue = read[0]
+            elif isinstance(read[1], numbers.Number):
+                readValue = read[1]
+            
+            if readValue >= 0:    
+                self.latestValue = readValue
+                self.array[self.currentIndex()] = readValue
+                self.incrementIndex()
+                self.calcAvgMaxMin()
+                
+            print self.plc.name + ': Value: ' + str(self.latestValue) + '. Avg: ' + str(self.avg) + '. Min: ' + str(self.min) + '. Max: ' + str(self.max)
             time.sleep(1)
             
     def stop(self):
         self.alive = False
+    
+    def currentIndex(self):
+        return self.index if self.index < READS_TO_LOG else self.index - READS_TO_LOG
+    
+    def calcCurrentValues(self):
+        return self.index if self.index < READS_TO_LOG else READS_TO_LOG
+    
+    def incrementIndex(self):
+        self.index = self.index + 1
+        self.index = self.index if self.index < READS_TO_LOG * 2 else self.index - READS_TO_LOG
+
+    
+    def calcAvgMaxMin(self):
+        total = 0
+        currentMax = self.array[0]
+        currentMin = self.array[0]
+        
+        for x in range(0, self.calcCurrentValues()):
+            currentValue = self.array[x]
+            if currentValue > currentMax:
+                currentMax = currentValue
+            if currentValue < currentMin:
+                currentMin = currentValue
+            total += currentValue
+            
+        self.max = currentMax
+        self.min = currentMin
+        self.avg = total / (self.calcCurrentValues())      
         
